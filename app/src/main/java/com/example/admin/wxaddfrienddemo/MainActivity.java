34c823716811d9e74647c903cd6c541ba5c65c86
carrys17 <961513094@qq.com>
2017-11-14 21:35:50 +0800

    .增加新的两种添加场景

--- app/src/main/java/com/example/admin/wxaddfrienddemo/MainActivity.java
+++ app/src/main/java/com/example/admin/wxaddfrienddemo/MainActivity.java
@@ -132,14 +132,14 @@ import java.util.List;
 //                     "--ez Is_RoomOwner false "+                 // 全为false 一开始猜测是否是群主，但是结果不是。
 //                     "--es Contact_RoomNickname  \"吃瓜的鸿\" "+        // 群昵称,可修改，去掉不显示
                      "--es Contact_User \"wxid_n24mdcaqj37s22\" "+  // 微信id，只给这个就可以跳到加人界面了
-                     "--ei Contact_Scene 15 "  +// 微信号搜索的为3，手机号搜索为15,群的方式是14
+                     "--ei Contact_Scene 3 "  +// 微信号搜索的为3，手机号搜索为15,群的方式是14
 //                     "--ez Contact_NeedShowChangeSnsPreButton false "  + // 都是false，可以去掉
                      // 第二种是点击右上角显示群信息后再点击群员到加人的界面，多了如下三个字段
 //                     "--es Contact_ChatRoomId \"636235605@chatroom\" "+
 //                     "--es Contact_Nick \"Hong\" "+    // 昵称
 //                     "--ez Contact_RoomMember true "+   // 全为true
                      "";
-             Runtime.getRuntime().exec(s1);
+//             Runtime.getRuntime().exec(s1);
 
             // 通讯录方式
              //I/set     ( 2625): bundle = Bundle[{Contact_NeedShowChangeRemarkButton=false,
@@ -150,19 +150,68 @@ import java.util.List;
 
              String s2 = "adb shell am start -n com.tencent.mm/com.tencent.mm.plugin.profile.ui.ContactInfoUI "+
                      "--es Contact_Mobile_MD5 \"270bae1b55a890759987f9047b81c027\" "+      // 手机号码的MD5值
-                     "--ez Contact_ShowUserName true "+                 // 全为false
+                     "--ez Contact_ShowUserName false "+                 // 全为false
 //                     "--es Contact_Signature  \"boeey\" "+                 // 个性签名
                      "--es Contact_Nick \"桌球\" "+                       // 昵称
                      // v1 值，只给这个就可以跳到加人界面了
                      "--es Contact_User \"v1_f2beaa1797ef0252d0421242b04ab14613f59c18c49bab2e395fc26badaf517a@stranger\" "+
                      "--ei Contact_Scene 13 "  +                          // 微信号搜索的为3，手机号搜索为15,群的方式是14，通讯录是13
-                     "--ei Contact_Sex 1 " +                              // 性别，1为男，0为女
+                     "--ei Contact_Sex 1 " +                              // 性别，1为男，2为女
                      "--ez Contact_NeedShowChangeSnsPreButton false "  + // 都是false，可以去掉
-
-
                      "";
           //   Runtime.getRuntime().exec(s2);
 
+
+             // 名片的方式,有一个细节，就是推名片的时候，推荐人可以看到被推荐人的wxid，收信息的人看不到
+            // I/set     ( 1116): intent = Intent { cmp=com.tencent.mm/.plugin.profile.ui.ContactInfoUI (has extras) }
+            // I/set     ( 1116): bundle = Bundle[{Contact_NeedShowChangeRemarkButton=false, Contact_QuanPin=莎雯[西瓜],
+             // Contact_Mobile_MD5=null, Contact_PyInitial=SWXG, Contact_ShowUserName=true, Contact_BrandIconURL=,
+             // Contact_Source_FMessage=17, source_from_user_name=wxid_lnt4ahvg2esr22, Contact_KSnsIFlag=0, Contact_Signature=,
+             // Contact_Province=, Contact_FMessageCard=true, User_From_Fmessage=false, Contact_VUser_Info=, source_from_nick_name=林,
+             // Contact_City=, Contact_Nick=莎雯[西瓜],
+             // Contact_User=v1_3030c5f2180425b41b787251deae1f25f4c3b9135b75109c44248cd1f245241ff77aedf1ef2502d629fd7eadaa543a71@stranger,
+             // Contact_VUser_Info_Flag=0, Contact_Sex=2, Contact_Uin=0, Contact_Alias=, Contact_Scene=17, Contact_RemarkName=null,
+             // Contact_full_Mobile_MD5=null, Contact_QQNick=0,
+             // AntispamTicket=v2_f04caa0fcae53fdb9ace3bb69bf1c94ed9ed7bb05f97563a12d2517e9f04115082978692df51d276b522ca1b06060be6@stranger,
+             // Contact_NeedShowChangeSnsPreButton=false}]
+
+             String s3 = "adb shell am start -n com.tencent.mm/com.tencent.mm.plugin.profile.ui.ContactInfoUI "+
+//                     "--ez Contact_ShowUserName true "+                 // 前面的全为false，这种模式为true
+//                     "--es source_from_user_name \"wxid_lnt4ahvg2esr22\" "+      // 推荐人的微信id
+//                     "--es source_from_nick_name \"林\" "+     // 推荐人的昵称
+//                     "--es Contact_Nick \"莎雯[西瓜]\" "+
+//                     // v1 值，被推荐人的v1 值。只给这个就可以跳到加人界面了
+                     "--es Contact_User \"v1_3030c5f2180425b41b787251deae1f25f4c3b9135b75109c44248cd1f245241ff77aedf1ef2502d629fd7eadaa543a71@stranger\" "+
+                     // 新的字段。v2值
+//                     "--es AntispamTicket \"v2_f04caa0fcae53fdb9ace3bb69bf1c94ed9ed7bb05f97563a12d2517e9f04115082978692df51d276b522ca1b06060be6@stranger\" "+
+                     "--ei Contact_Scene 3 "  +                          // 微信号搜索的为3，手机号搜索为15,群的方式是14，通讯录是13，名片为17,qq号是12
+//                     "--ei Contact_Sex 2 " +                              // 性别，1为男，2为女,0为未设置
+//                     "--ez Contact_NeedShowChangeSnsPreButton false "  + // 都是false，可以去掉
+                     "";
+//             Runtime.getRuntime().exec(s3);
+
+
+             // I/set ( 1517): bundle = Bundle[{Contact_NeedShowChangeRemarkButton=false,
+             // Contact_QuanPin=wxidqxve6fsx4ery12, Contact_PyInitial=WXIDQXVE6FSX4ERY12,
+             // Contact_RegionCode=CN_Guangdong_Guangzhou, Contact_BrandList=<brandlist count="0"
+             // ver="684775412"></brandlist>, Contact_KSnsBgId=0, Contact_IsLBSFriend=true, Contact_KWeiboNick=null,
+             // Contact_KSnsBgUrl=null, Contact_KSnsIFlag=0, Contact_KWeibo_flag=0, Contact_Signature=宅若久时天然呆, 呆到深处自然萌,
+             // Contact_BIZ_KF_WORKER_ID=null, Contact_VUser_Info=null, Contact_KHideExpose=true, Contact_Nick=雯雯,
+             // Contact_User=v1_4b6ece89d367aed49ce765008cd11b297b3c3836bbc71c70ad71dc3e5da1b2359ee868dfe958ef76121ce676f53ab246@stranger,
+             // Contact_VUser_Info_Flag=0, Contact_Sex=2, Contact_Alias=aa15596417033, Contact_Scene=30,
+             // Contact_KWeibo=null, AntispamTicket=v2_973dea324a20f5da3d5de134c8c725d768ffb73ba1ec057d61c89f3140fb128e211dd1239af7f458ef7d20df144fcc76@stranger,
+             // Contact_NeedShowChangeSnsPreButton=false}]
+             String s4 = "adb shell am start -n com.tencent.mm/com.tencent.mm.plugin.profile.ui.ContactInfoUI "+
+
+//                     // v1 值，只给这个就可以跳到加人界面了,可以替换为微信id
+                     "--es Contact_User \"v1_4b6ece89d367aed49ce765008cd11b297b3c3836bbc71c70ad71dc3e5da1b2359ee868dfe958ef76121ce676f53ab246@stranger\" "+
+                     // 新的字段。v2值
+//                     "--es AntispamTicket \"v2_f04caa0fcae53fdb9ace3bb69bf1c94ed9ed7bb05f97563a12d2517e9f04115082978692df51d276b522ca1b06060be6@stranger\" "+
+                     "--ei Contact_Scene 30 "  +                          // 微信号搜索的为3，手机号搜索为15,群的方式是14，通讯录是13，名片为17,qq号是12，扫一扫是30
+//                     "--ei Contact_Sex 2 " +                              // 性别，1为男，2为女,0为未设置
+//                     "--ez Contact_NeedShowChangeSnsPreButton false "  + // 都是false，可以去掉
+                     "";
+             Runtime.getRuntime().exec(s4);
          } catch (IOException e) {
              e.printStackTrace();
          }
